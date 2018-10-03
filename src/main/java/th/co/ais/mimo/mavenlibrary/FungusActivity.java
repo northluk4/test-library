86d9d935169b361fb11a1765233b186a3260331c
northluk4 <vasic171@ais.co.th>
2018-10-03 14:50:27 +0700

    Intial Test

---
+++ src/main/java/th/co/ais/mimo/mavenlibrary/FungusActivity.java
@@ -0,0 +1,12 @@
+package th.co.ais.mimo.mavenlibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FungusActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_library_activity_main);
+    }
+}
